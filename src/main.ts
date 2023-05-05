02bf6ccf774d43afdb544db2338d3dca50bef78d
JiHoon-0330 <ljihoon0330@gamil.com>
2023-05-05 14:25:46 +0900

    gzip

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { HttpExceptionFilter } from "./middleware/http.exception.filter";
 import { NestFactory } from "@nestjs/core";
+import compression from "compression";
 import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./middleware/http.exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.getHttpAdapter().getInstance().set("etag", false);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(compression());
 
   await app.listen(3000);
 }
